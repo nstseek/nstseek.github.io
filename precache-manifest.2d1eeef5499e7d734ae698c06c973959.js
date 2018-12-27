d43600d9330f16fa58f67df3a963bce44994fc57
Rafael Lopes Schneider <rafael.rls70@gmail.com>
2018-12-27 00:56:40 -0200

    uploading new react site

---
+++ precache-manifest.2d1eeef5499e7d734ae698c06c973959.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = [
+  {
+    "revision": "2c12d4291c7cf3aebe3c28fdaa97f09d",
+    "url": "/static/media/me.2c12d429.jpg"
+  },
+  {
+    "revision": "a968f33afb10b63d03b7ff3ac27a73a6",
+    "url": "/static/media/lattes.a968f33a.jpg"
+  },
+  {
+    "revision": "784f355ed9a18d22a761f1acfac72a1b",
+    "url": "/static/media/insta.784f355e.jpeg"
+  },
+  {
+    "revision": "5a6a8ac192e4c4cc6f96441b0b1b5079",
+    "url": "/static/media/background.5a6a8ac1.JPG"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "88bb8e6499261ef548a2",
+    "url": "/static/js/main.88bb8e64.chunk.js"
+  },
+  {
+    "revision": "c47e67fe727a3909d5a7",
+    "url": "/static/js/1.c47e67fe.chunk.js"
+  },
+  {
+    "revision": "88bb8e6499261ef548a2",
+    "url": "/static/css/main.c4d6fb6d.chunk.css"
+  },
+  {
+    "revision": "d19dfac243ebd368e32dae2d44de35f5",
+    "url": "/index.html"
+  }
+];
