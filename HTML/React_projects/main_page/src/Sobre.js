e14a2e2713e4c359e72d5788fba9db06113ea730
Rafael Lopes Schneider <rafael.rls70@gmail.com>
2018-12-30 01:39:39 -0200

    main site facelift w/ new functions

---
+++ HTML/React_projects/main_page/src/Sobre.js
@@ -0,0 +1,21 @@
+import React from 'react';
+import './Sobre.css';
+
+class Sobre extends React.Component{
+
+    render(){
+        return (
+            <div className="Sobre">
+                <div id="sobreTitle">
+                    <h1>Sobre mim</h1>
+                </div>
+                <div id="sobreContent">
+                    <p>Tenho experiência como web developer full stack, C, C++, Javascript, Bootstrap, jQuery, React, Node.js, PL/SQL, PostgreSQL, HTML5, CSS3 ,designer gráfico, desenhista técnico (AutoCAD/SolidWorks), suporte técnico, montagem e manutenção de computadores, manutenção automotiva, manutenção e instalação elétrica, eletrônica, administração de redes e sistemas linux, automação com Arduino, participei de competições de robótica por hobby, venci algumas competições, possuo inglês fluente, tenho cursos profissionalizantes em manutenção de computadores ,design gráfico/web design dentre outros, atualmente estou cursando Engenharia da Computação na Pontifícia Universidade Católica do Rio Grande do Sul, possuo vários projetos em meu GitHub, projetos desenvolvidos durante minha aprendizagem que se encontram em <a target="_blank" rel="noopener noreferrer" href="https://github.com/nstseek/nstseek.github.io">https://github.com/nstseek/nstseek.github.io</a></p> 
+                </div>
+            </div>
+        );
+    }
+
+}
+
+export default Sobre;
