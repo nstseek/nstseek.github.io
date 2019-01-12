d752bed26deae80a539f6a75291866cef2c4a62b
Rafael Lopes Schneider <rafael.rls70@gmail.com>
2019-01-12 10:14:20 -0200

    uploading build

---
+++ HTML/React_projects/smartbrain/build/precache-manifest.7cc6e675377fd5bab40949659b5c387c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "d7d5897a74bdc190ada5",
+    "url": "/static/js/main.d7d5897a.chunk.js"
+  },
+  {
+    "revision": "e496d77497549368830a",
+    "url": "/static/js/1.e496d774.chunk.js"
+  },
+  {
+    "revision": "d7d5897a74bdc190ada5",
+    "url": "/static/css/main.2d04521c.chunk.css"
+  },
+  {
+    "revision": "e496d77497549368830a",
+    "url": "/static/css/1.da4e4c74.chunk.css"
+  },
+  {
+    "revision": "958452138ce047bb0d7259a04692f856",
+    "url": "/index.html"
+  }
+];
