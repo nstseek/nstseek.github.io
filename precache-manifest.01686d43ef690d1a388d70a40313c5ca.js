6bc668ed1073c933a43f4763157bea6e2f23817b
Rafael Lopes Schneider <rafael.rls70@gmail.com>
2019-01-12 11:03:55 -0200

    updating mainpage

---
+++ precache-manifest.01686d43ef690d1a388d70a40313c5ca.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = [
+  {
+    "revision": "d66f3fde06551afdf4bda27e20a8d054",
+    "url": "/static/media/folder2.d66f3fde.png"
+  },
+  {
+    "revision": "8e343354c2f664d73830",
+    "url": "/static/css/main.b1e43734.chunk.css"
+  },
+  {
+    "revision": "10f5598a7ed010677e8c",
+    "url": "/static/js/1.10f5598a.chunk.js"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "49ede5fb772c914cc4e8d5bab22b1d6f",
+    "url": "/static/media/me.49ede5fb.png"
+  },
+  {
+    "revision": "8e343354c2f664d73830",
+    "url": "/static/js/main.8e343354.chunk.js"
+  },
+  {
+    "revision": "e4e84aa836c6bde004db4a57ec33c55f",
+    "url": "/static/media/insta.e4e84aa8.png"
+  },
+  {
+    "revision": "a1a8eee2f4b67ae9df5a2458dccf4dd1",
+    "url": "/static/media/git2.a1a8eee2.png"
+  },
+  {
+    "revision": "8c3378965e48acaf628456e9b06a2f8b",
+    "url": "/static/media/lattes.8c337896.png"
+  },
+  {
+    "revision": "5a6a8ac192e4c4cc6f96441b0b1b5079",
+    "url": "/static/media/background.5a6a8ac1.JPG"
+  },
+  {
+    "revision": "e9ee4834865cc18ff9dd459df72d8dd1",
+    "url": "/index.html"
+  }
+];
