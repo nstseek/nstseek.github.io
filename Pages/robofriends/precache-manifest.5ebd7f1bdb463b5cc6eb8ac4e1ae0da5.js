b09491b887cf5963ca8b3fede42a8c7c47bcb3c7
Rafael Lopes Schneider <rafael.rls70@gmail.com>
2019-01-12 10:33:47 -0200

    uploaading pages

---
+++ Pages/robofriends/precache-manifest.5ebd7f1bdb463b5cc6eb8ac4e1ae0da5.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "12a3db8ba7f5423327bb12aed3c41977",
+    "url": "/Pages/robofriends/static/media/SEGA.12a3db8b.woff"
+  },
+  {
+    "revision": "24f14174bace2890f201",
+    "url": "/Pages/robofriends/static/js/runtime~main.24f14174.js"
+  },
+  {
+    "revision": "7855d5acc779f1fe85bf",
+    "url": "/Pages/robofriends/static/js/main.7855d5ac.chunk.js"
+  },
+  {
+    "revision": "a93dcc462ebf1e8f3aeb",
+    "url": "/Pages/robofriends/static/js/1.a93dcc46.chunk.js"
+  },
+  {
+    "revision": "7855d5acc779f1fe85bf",
+    "url": "/Pages/robofriends/static/css/main.46e45dbd.chunk.css"
+  },
+  {
+    "revision": "a93dcc462ebf1e8f3aeb",
+    "url": "/Pages/robofriends/static/css/1.e16439e5.chunk.css"
+  },
+  {
+    "revision": "2c183ecba8430734a1b57b25852add7a",
+    "url": "/Pages/robofriends/index.html"
+  }
+];
---
+++ Pages/smartbrain/precache-manifest.f8f55b2353b25a00ce5e8b3be18f85dc.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "82e094937082b7da2c8f",
+    "url": "/Pages/smartbrain/static/js/runtime~main.82e09493.js"
+  },
+  {
+    "revision": "2b588846525b664572cf",
+    "url": "/Pages/smartbrain/static/js/main.2b588846.chunk.js"
+  },
+  {
+    "revision": "de2483cdc5be06c9b206",
+    "url": "/Pages/smartbrain/static/js/1.de2483cd.chunk.js"
+  },
+  {
+    "revision": "2b588846525b664572cf",
+    "url": "/Pages/smartbrain/static/css/main.2d04521c.chunk.css"
+  },
+  {
+    "revision": "de2483cdc5be06c9b206",
+    "url": "/Pages/smartbrain/static/css/1.da4e4c74.chunk.css"
+  },
+  {
+    "revision": "ef118d5ab08f5462e5df1b309df9a0ac",
+    "url": "/Pages/smartbrain/index.html"
+  }
+];
