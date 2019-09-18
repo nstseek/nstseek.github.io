4dea65ce7c6134c1a964cce4cfa925865482884d
Rafael Lopes Schneider <rafael.rls70@gmail.com>
2019-09-18 13:41:31 -0300

    Adds new page

---
+++ Pages/webapp-hackatona-2019/precache-manifest.574485a7259dab02107a14ff1a49acdf.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "96bb48259b26c0b461efd0bedfb725a1",
+    "url": "/index.html"
+  },
+  {
+    "revision": "094c4721adcda1151b81",
+    "url": "/static/css/main.3520d55a.chunk.css"
+  },
+  {
+    "revision": "2543f6d6c196c55fef13",
+    "url": "/static/js/2.5dc9f9d2.chunk.js"
+  },
+  {
+    "revision": "094c4721adcda1151b81",
+    "url": "/static/js/main.0ae9ac2f.chunk.js"
+  },
+  {
+    "revision": "a50032bb848ae1291342",
+    "url": "/static/js/runtime~main.b249e9f6.js"
+  },
+  {
+    "revision": "f937cbfaa00d030981f22810eedafe17",
+    "url": "/static/media/Logo.f937cbfa.png"
+  }
+]);
