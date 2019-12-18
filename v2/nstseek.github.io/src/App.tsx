7b8f7e3d04a52c0ad6db524910a58c71bb23bd4e
Rafael Lopes Schneider <rafael.rls70@gmail.com>
2019-12-18 16:23:27 -0300

    Adiciona rotas

--- v2/nstseek.github.io/src/App.tsx
+++ v2/nstseek.github.io/src/App.tsx
@@ -1,25 +1,48 @@
 import React from 'react';
+import { BrowserRouter as Router, Link, Redirect, Route, Switch } from 'react-router-dom';
 import './App.scss';
-import logo from './logo.svg';
 
 const App: React.FC = () => {
   return (
-    <div className='App'>
-      <header className='App-header'>
-        <img src={logo} className='App-logo' alt='logo' />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className='App-link'
-          href='https://reactjs.org'
-          target='_blank'
-          rel='noopener noreferrer'
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <Switch>
+        <Route path='/inicio'>
+          <span>Principal</span>
+        </Route>
+        <Route path='/sobre'>
+          <span>Sobre</span>
+        </Route>
+        <Route path='/repositorio'>
+          <span>Repositório</span>
+        </Route>
+        <Route path='/paginas'>
+          <span>Páginas</span>
+        </Route>
+        <Route path='/contato'>
+          <span>Contato</span>
+        </Route>
+        <Route path='/rotas'>
+          <Link to='/inicio'>
+            <span>xama no inicio</span>
+          </Link>
+          <Link to='/sobre'>
+            <span>xama no sobre</span>
+          </Link>
+          <Link to='/repositorio'>
+            <span>xama no repositorio</span>
+          </Link>
+          <Link to='/paginas'>
+            <span>xama nas paginas</span>
+          </Link>
+          <Link to='/contato'>
+            <span>xama no contato</span>
+          </Link>
+        </Route>
+        <Route path='*'>
+          <Redirect to='/rotas'/>
+        </Route>
+      </Switch>
+    </Router>
   );
 };
 
