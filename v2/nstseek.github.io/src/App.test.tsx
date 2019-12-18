069718fc56d5ab104e9e0d04819af7345da77db8
Rafael Lopes Schneider <rafael.rls70@gmail.com>
2019-12-18 14:30:53 -0300

    Adiciona linter

--- v2/nstseek.github.io/src/App.test.tsx
+++ v2/nstseek.github.io/src/App.test.tsx
@@ -1,6 +1,6 @@
+import { shallow } from 'enzyme';
 import React from 'react';
 import App from './App';
-import { shallow } from 'enzyme';
 
 test('renders learn react link', () => {
   shallow(<App />);
